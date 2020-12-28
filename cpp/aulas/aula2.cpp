c6f6c61a3cd030c4b7180cdb59c3141240817d58
charlesdev771 <charlesdev77021@gmail.com>
2020-12-28 04:40:46 -0300

    cpp 2

--- cpp/aulas/aula2.cpp
+++ cpp/aulas/aula2.cpp
@@ -1,11 +1,33 @@
 #include <iostream>
 #include <stdio.h>
+#include <iomanip>>
 
 using namespace std;
 
 int main()
 {
     cout << "HELLO WORLD" << endl;
+
+    int num1 = 11;
+    float num2 = 7;
+    int num3;
+
+    if(num1 > num2) {
+        cout << "Eh maior" << endl;
+    }else{
+        cout << "Nao eh maior" << endl;
+    }
+    cout << "Entre com um valor: " << endl;
+    cin >> num3;
+
+    for(int i = 0; i <= 10; i++) {
+        cout << "Iterador: " << i << endl;
+    }
+
+    while(num1 <= 100) {
+        cout << "Hello" << endl;
+        num1++;
+    }
 
     return 0;
 }
